5ac4fc50755b2aba1d8355af0323cb9f86fdb92d
Vanilson <vanilson@NOTEVK.progressul.local>
2022-08-11 23:42:25 -0300

    aula03
    Configuração e liberação do unrestricted para criação dos arquivos tsconfig.
    Efetuada a criação e teste do arquivo app.ts

--- exemplos/validacao-basica.js
+++ exemplos/validacao-basica.js
@@ -1,7 +1,6 @@
-let button = document.getElementById('button');
+/*let button = document.getElementById('button');
 let input1 = document.getElementById('input1');
 let input2 = document.getElementById('input2');
-
 
 function somarNumeros(numero1, numero2){
     if(typeof numero1 === 'number' && typeof numero2 ==='number'){
@@ -14,3 +13,5 @@ function somarNumeros(numero1, numero2){
 button.addEventListener('click', () => {
     console.log(somarNumeros(input1.value, input2.value));
 })
+
+*/
