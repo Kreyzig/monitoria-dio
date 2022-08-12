38dd5a25a6dcbc4cb69dac92ebcf8fda93121596
Vanilson <vanilson@NOTEVK.progressul.local>
2022-08-11 23:52:10 -0300

    aula03
    tipos de variaveis

--- app.js
+++ app.js
@@ -2,13 +2,19 @@
 let button = document.getElementById('button');
 let input1 = document.getElementById('input1');
 let input2 = document.getElementById('input2');
-function adicionarNmero(numero1, numero2) {
+function adicionarNmero(numero1, numero2, devPrintar, frase) {
+    let resultado = numero1 + numero2;
+    if (devPrintar) {
+        console.log(frase + resultado);
+    }
     return numero1 + numero2;
 }
+let devPrintar = true;
+let frase = 'O valor é : ';
 if (button) {
     button.addEventListener('click', () => {
         if (input1 && input2) {
-            console.log(adicionarNmero(Number(input1.value), Number(input2.value)));
+            console.log(adicionarNmero(Number(input1.value), Number(input2.value), devPrintar, frase));
         }
     });
 }
--- app.ts
+++ app.ts
@@ -2,14 +2,22 @@ let button = document.getElementById('button');
 let input1 = document.getElementById('input1') as HTMLInputElement;
 let input2 = document.getElementById('input2') as HTMLInputElement;
 
-function adicionarNmero(numero1:number, numero2: number){
+function adicionarNmero(numero1:number, numero2: number, devPrintar:boolean, frase:string){
+
+    let resultado= numero1 + numero2
+
+    if(devPrintar){
+        console.log(frase + resultado)
+    }
     return numero1 + numero2;
 }
 
+let devPrintar = true;
+let frase = 'O valor é : ';
 if(button){
     button.addEventListener('click', () => {
         if(input1 && input2){
-            console.log(adicionarNmero(Number(input1.value), Number(input2.value)))
+            console.log(adicionarNmero(Number(input1.value), Number(input2.value),devPrintar, frase))
         }
     })
 }
